40b11603f3739b208f76239ac976ac40a2cc4fb1
Immaculeta1 <immaculetapascaline@gmail.com>
2022-06-03 10:47:36 +0100

    ecommerce page

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://media.istockphoto.com/photos/attractive-woman-posing-in-a-pink-dress-picture-id164125799?k=20&m=164125799&s=612x612&w=0&h=pes7a0mC2u8auFvLbO_fO7jimzW5DotSNeP0UM-VOhQ=",
+        title: "SUMMER SALE",
+        desc:  "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fsfafd"
+    },
+    {
+        id: 2,
+        img: "https://media.istockphoto.com/photos/attractive-woman-posing-in-a-pink-dress-picture-id164125799?k=20&m=164125799&s=612x612&w=0&h=pes7a0mC2u8auFvLbO_fO7jimzW5DotSNeP0UM-VOhQ=",
+        title: "WINTER SALE",
+        desc:  "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://media.istockphoto.com/photos/attractive-woman-posing-in-a-pink-dress-picture-id164125799?k=20&m=164125799&s=612x612&w=0&h=pes7a0mC2u8auFvLbO_fO7jimzW5DotSNeP0UM-VOhQ=",
+        title: "POPULAR SALE",
+        desc:  "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbfof4"
+   },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1434510423563-c7e99bbc5bbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1565537222174-2a43ca1c3462?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1553591589-2e96ef7eca65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+        title: "DENIM JACKET",
+    }
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://media.istockphoto.com/photos/blank-henley-tshirt-mockup-front-view-design-presentation-for-print-picture-id1309834855?k=20&m=1309834855&s=612x612&w=0&h=vSeK0sVbQEzJEG9BmJjyhs-A6rQtWeY5-M-BEDiBwec=",
+    },
+    {
+        id: 2,
+        img: "https://media.istockphoto.com/photos/black-tshirt-on-a-white-background-picture-id187300373?k=20&m=187300373&s=612x612&w=0&h=wycH4FwYSSOeUADIGP8oo16IXQR0lS15nI5hHOOY9Bw=",
+    },
+    {
+        id: 3,
+        img: "https://media.istockphoto.com/photos/mens-brown-wrinkled-shorts-with-belt-isolated-on-white-background-picture-id505562951?k=20&m=505562951&s=612x612&w=0&h=KdULAxxnRShvUojQPCFHu4aRIEtYN291Jo1i8OdOR5w=",
+    },
+    {
+        id: 4,
+        img: "https://media.istockphoto.com/photos/flowery-evase-sweetheart-dress-picture-id178852003?k=20&m=178852003&s=612x612&w=0&h=YcNA59LTF-Euk5Eo8fNpDVlUYpBDxgp9Nzf3ozDWrLQ=",
+    },
+    {
+        id: 5,
+        img: "https://media.istockphoto.com/photos/handbag-in-studio-picture-id615523886?k=20&m=615523886&s=612x612&w=0&h=oX47NZE75LBcSTxXuM5IO2eqzc0ul-5SMykA1GgQVug=",
+    },
+    {
+        id: 6,
+        img: "https://media.istockphoto.com/photos/blank-baseball-cap-color-darkgrey-picture-id664895468?k=20&m=664895468&s=612x612&w=0&h=M07beVEE5_7veG2aOeUUSyok2UrUouABinnXCfzYJKw=",
+    },
+    {
+        id: 7,
+        img: "https://media.istockphoto.com/photos/elegant-designer-fur-lined-ladies-winter-jacket-picture-id907852976?k=20&m=907852976&s=612x612&w=0&h=zz_ComFCh7Ro33mSITzoYqx1tBjgWsVRcMMYpMuwQYU=",
+    },
+    {
+        id: 8,
+        img: "https://media.istockphoto.com/photos/woman-winter-clothes-picture-id97929910?k=20&m=97929910&s=612x612&w=0&h=HwJeK6_XlbVCLrEht43-xxIodQ7RSQqRQy9jerA5108=",
+    }
+];
