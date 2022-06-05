2909863f75118e5f99c63383e4a63f7d27fd85bc
Immaculeta1 <immaculetapascaline@gmail.com>
2022-06-05 11:04:58 +0100

    Single product page added

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@material-ui/icons";
 import styled from "styled-components";
 import Announcement from "../Components/Announcement";
 import Footer from "../Components/Footer";
@@ -5,13 +6,161 @@ import Navbar from "../Components/Navbar";
 import Newsletter from "../Components/Newsletter";
 
 
-const Container = styled.div``;
+const Container = styled.div`
+`;
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`;
+
+const ImgContainer = styled.div`
+    flex: 1;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit:cover;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+    font-weight: 200;
+`;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    fontweight: 200;
+`;
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props=>props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`;
+
+const FilterSizeOption = styled.option`
+`;
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 10px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover {
+        background-color: #f8f4f4;
+    }
+`;
+
+
 
 const Product = () => {
   return (
     <Container>
         <Navbar/>
         <Announcement/>
+        <Wrapper>
+            <ImgContainer>
+                <Image src="https://media.istockphoto.com/photos/happy-young-woman-standing-with-hands-in-pockets-picture-id157662202?k=20&m=157662202&s=612x612&w=0&h=yNakF-PkmYYsTJ8qu-IUDAHE2CzME5DVEyolEhSjL4w=" />
+            </ImgContainer>
+            <InfoContainer>
+               <Title>Denim Jumpsuit</Title> 
+               <Desc>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo iaculis 
+               gravida. Sed leo neque, aliquam rhoncus tortor vitae, molestie cursus neque. 
+               Etiam a malesuada ligula. In turpis urna, lacinia nec augue et, placerat euismod 
+               odio. In eleifend augue turpis. Maecenas at nulla id nisi posuere rutrum ac sed metus. 
+               Mauris accumsan ligula vitae odio maximus, eu sagittis nisl fringilla. Mauris mattis.</Desc>
+                <Price>$ 20</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Color</FilterTitle>
+                        <FilterColor color="black"/>
+                        <FilterColor color="darkblue"/>
+                        <FilterColor color="gray"/>
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
+                </FilterContainer>
+                <AddContainer> 
+                    <AmountContainer>
+                       <Remove/>
+                       <Amount>1</Amount>
+                       <Add/>
+                    </AmountContainer>
+
+                    <Button>ADD TO CART</Button>
+                </AddContainer>
+               
+            </InfoContainer>
+        </Wrapper>
         <Newsletter/>
         <Footer/>
     </Container>
